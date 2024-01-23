df0a21fec7309da3d1f6397c44c58ade92075e8a
mooinida <andls4567@naver.com>
2024-01-23 15:41:33 +0900

    mine

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'; //ES6
+import { response } from './config/response.js';
+import { tempRouter } from './src/routes/temp.route.js';
+import { BaseError } from './config/error.js';
+import { status } from './config/response.status.js';
+
+const app = express();
+const port = 3000;
+
+app.use('/temp',tempRouter);
+
+app.use((req, res, next) => {
+    const err = new BaseError(status.NOT_FOUND);
+    next(err);
+});
+
+app.use((err,req,res,next)=>{
+    res.locals.message=err.message;
+    res.locals.error=process.env.NODE_ENV!=='production'?err:{};
+    res.status(err.data.status).send(response(err.data));
+});
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`);
+})
